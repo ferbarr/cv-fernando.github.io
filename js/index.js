bacf4dd40b6867bd16675803f04e0cfc5498802e
ferbarr <barrerarosalesfernando8@gmail.com>
2023-05-09 14:57:15 -0600

    version2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-// Seleccionar formulario e input
-const form = document.querySelector("#formulario"); //Mandar llamar al formulario
-const input = document.querySelector("#comentario"); //Mandar llamar al input
-const boton = document.querySelector("#boton"); //Mandar llamar al boton
+const form = document.querySelector("#formulario"); //Seleccionar formulario y guardar sus valores en la constante form
+const input = document.querySelector("#comentario"); //Seleccionar input y guardar sus valores en la constante input
+const boton = document.querySelector("#boton"); //Seleccionar boton y guardar sus valores en la constante boton
 
-form.addEventListener("submit", async (e) => {
-  // Agregra evento al formulario de tipo submit
-  e.preventDefault(); //Para evitar que se recargue la pagina
-  const valor = input.value; //Guardar el valor escrito en el input
+form.addEventListener("submit", async (e) => {// Agregra evento al formulario de tipo submit que es para mandar datos
+  
+  e.preventDefault(); //Para evitar que se recargue la pagina que es lo que hace por defecto el evento submit
+  const valor = input.value; //Obtenemos el valor escrito en el input y lo guardamos en la constante valor
+  if(!valor){
+    alert('Primero ingresa un comentario');
+    return
+
+  }
   try {
     boton.disabled = true; //Deshabilitamos el boton
     const res = await fetch(
@@ -17,14 +21,15 @@ form.addEventListener("submit", async (e) => {
         headers: {
           "Content-Type": "application/json", //Indicamos el tipo de datos
         },
-        body: JSON.stringify(valor), //Mandamos el valor de input en formato JSON
+        body: JSON.stringify(valor), //Mandamos el valor del input en formato JSON
       }
     );
     input.value = ""; //Vaciamos el valor del input
-    alert("Gracias por sus comentarios"); //Mostramos alerta
+    alert("Gracias por sus comentarios, los revisaré"); //Mostramos alerta
   } catch (e) {
     //En caso de error lo muestra en la consola
     console.log(e);
+    alert('Ocurrió un error, vuelva a intentarlo')
   } finally {
     boton.disabled = false; //Habilitamos otra vez el boton
   }
